7f439f1f122c077a80c200a2a9df64e5dd60638c
ljobeda11-stack <ljobeda11@gmail.com>
2025-12-17 05:46:31 +0000

    Add android folder

---
+++ android/app/src/main/java/com/eduyarnai/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.eduyarnai.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.eduyarnai.app',
+  appName: 'copy-of-eduvoice-ai',
+  webDir: 'dist'
+};
+
+export default config;
